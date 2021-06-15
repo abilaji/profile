2218601b872359a963184d8e353f511cc875ccb2
unknown <abilaji96@gmail.com>
2021-06-15 11:59:36 +0700

    menu bar

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
-const togles = document.getElementById('menuTogle');
+const togles = document.querySelector('.menuTogle');
+const menuBar = document.querySelector('nav ul');
 
-togles.onclick= function(){
-  togles.classList.toggle('active');
-}
+togles.onclick = function(){
+  togles.classList.toggle('active')
+};
 
+togles.addEventListener("click", function(){
+  menuBar.classList.toggle('active');
+});
