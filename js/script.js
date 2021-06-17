20932e0737d78fb9e2399fb78b9037cdfe3cda49
unknown <abilaji96@gmail.com>
2021-06-17 16:33:07 +0700

    sticky

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 const togles = document.querySelector('.menuTogle');
 const menuBar = document.querySelector('nav ul');
+const nav = document.querySelector('nav');
+
+window.addEventListener('scroll', () => {
+  nav.classList.toggle('sticky', window.scrollY > 0)
+})
 
 togles.addEventListener('click', () => {
   togles.classList.toggle('active')
